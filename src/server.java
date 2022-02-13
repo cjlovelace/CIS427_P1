00aa39fe0307c0af1e435f9a9ae4148401b2939e
cjlovelace <50459158+cjlovelace@users.noreply.github.com>
2022-02-12 20:09:03 -0500

    Program successfully compares text login ids with input from client.

--- src/server.java
+++ src/server.java
@@ -17,6 +17,9 @@ public class server {
 
     public static void createCommunicationLoop() {
         try {
+            boolean loggedIn = false;
+            String userName = "";
+
             //create server socket
             ServerSocket serverSocket =
                     new ServerSocket(SERVER_PORT);
@@ -40,15 +43,23 @@ public class server {
             while(true) {
                 String[] strReceived = inputFromClient.readUTF().split(" ");
                 var strLength = strReceived.length;
-                outputToClient.writeUTF(Integer.toString(strLength));
-                if (attemptLogin(strReceived))
+                if (!loggedIn)
                 {
-                    outputToClient.writeUTF("Worked.");
-                }
-                else {
-                    outputToClient.writeUTF("Failed.");
-                };
+                    if (attemptLogin(strReceived)) {
+                        outputToClient.writeUTF("Successfully logged in.");
+                    }
 
+                    if (strLength != 3 || strReceived[0] != "LOGIN")
+                    {
+                        //outputToClient.writeUTF("FAILURE: Please provide correct username and password. Try again.");
+                    }
+                    else if (strReceived[0] == "LOGIN")
+                    {
+                        if (attemptLogin(strReceived)) {
+                            outputToClient.writeUTF("Successfully logged in.");
+                        }
+                    }
+                }
                 /**if(strReceived.equalsIgnoreCase("hello")) {
                     System.out.println("Sending hello to client");
                     outputToClient.writeUTF("hello client!");
@@ -66,11 +77,22 @@ public class server {
         try {
             File txtFile = new File("CIS427_P1/src/logins.txt");
             Scanner scan = new Scanner(txtFile);
-            boolean isValidLogin = false;
-            return true;
+
+            while (scan.hasNextLine())
+            {
+                String[] txtLine = scan.nextLine().split(" ");
+                System.out.println("From file: " + txtLine[0] + " " + txtLine[1]);
+                if (input[1].equals(txtLine[0]) && input[2].equals(txtLine[1]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
         catch(IOException ex) {
             ex.printStackTrace();
+            System.out.println("Unable to connect to logins.txt. Please check file path.");
             return false;
         }//end try-catch
 
